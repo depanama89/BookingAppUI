c4db2e3377e5d0188188401012ac2463dd2d4326
Patrick Kudiatu <kudiatutomanitu@gmail.com>
2025-06-11 20:39:55 +0200

    creation de la page homescreen

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -6,6 +6,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFFF2F2E6),
       appBar: AppBar(
         toolbarHeight: 90,
         backgroundColor: Colors.transparent,
@@ -13,8 +14,39 @@ class HomeScreen extends StatelessWidget {
           children: [
             IconButton(
               onPressed: () {},
-              icon: Icon(Icons.arrow_back),
+              icon: Icon(Icons.arrow_back_ios),
               style: IconButton.styleFrom(backgroundColor: Colors.white),
+              padding: EdgeInsets.all(20),
+            ),
+            Spacer(),
+            Container(
+              padding: EdgeInsets.all(2),
+              decoration: BoxDecoration(
+                color: Color(0xFFEAEAD2),
+                borderRadius: BorderRadius.circular(30),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withValues(alpha: 0.4),
+                    blurRadius: 4,
+                    spreadRadius: .82,
+                  ),
+                ],
+              ),
+              child: Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Color(0xFF051888),
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                    child: Text(
+                      "one way",
+                      style: TextStyle(fontSize: 16, color: Colors.white),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
